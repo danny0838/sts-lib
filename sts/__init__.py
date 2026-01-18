899cef95cf616b49be6d1b66312549bafa7ae1dc
Danny Lin <danny0838@gmail.com>
2026-01-18 09:00:56 +0800

    Release 0.35.1

--- sts/__init__.py
+++ sts/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """An open library for flexible simplified-traditional Chinese text conversion."""
-__version__ = '0.35.0'
+__version__ = '0.35.1'
 
 import html
 import itertools
